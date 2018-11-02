553150172492f097e44ea54db808c59ed57e30c9
TAMIR YARDENI <tamir.yardeni1@gmail.com>
2018-11-02 18:49:02 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { StartComponent } from './components/start/start.component';
+import { JoinComponent } from './components/join/join.component';
+import { GameComponent } from './components/game/game.component';
 
+const appRoutes: Routes = [
+  { path: 'join',       component: JoinComponent  },
+  { path: 'start',      component: StartComponent },
+  { path: '',           component: MainComponent  },
+  { path: 'game/:id',   component: GameComponent  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    StartComponent,
+    JoinComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
